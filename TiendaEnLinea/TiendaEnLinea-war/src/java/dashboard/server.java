cd54169ff5b9823b6dcd9650c1facb09af8ac539
victor <victor@victor-notebook>
2017-12-20 16:05:53 -0600

    Tienda en línea finaliza diopjavp0sdiavjhiasdnhv

--- TiendaEnLinea/TiendaEnLinea-war/src/java/dashboard/server.java
+++ TiendaEnLinea/TiendaEnLinea-war/src/java/dashboard/server.java
@@ -1,7 +1,7 @@
 package dashboard;
 public class server {
     //Direccion del servidor, pa que no alegues Tobias. 
-    static private String server = "jdbc:mysql://db-server/CRM?user=root&password=123456";
+    static private String server = "jdbc:mysql://db-server/CRM?user=crmusr&password=123456";
     public String getServer() {
         return server;
     }
